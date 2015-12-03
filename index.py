085bb2cbfff09983563ec9358c9035e10acd528f
pastoremetal <pastoremetal@yahoo.com.br>
2015-12-03 17:56:31 -0200

    Tiles

--- index.py
+++ index.py
@@ -9,20 +9,22 @@ class stage(object):
                "realY": 0,
                "adj": {}
                }
-    avatar = {"posi": {"x": 0,
-                       "y": 0,
-                       },
+    avatar = {"posi": {"x": 0, "y": 0,},
               "sprite" :{
                       "dimensions": {"w": 832, "h": 1344},
                       "nums":{"x": 13, "y": 21},
-                      "file": pygame.transform.scale(pygame.image.load("imgs/avatar/e9Zh9HxR.png"), (832, 1344))        
+                      "file": pygame.transform.scale(pygame.image.load("imgs/avatar/e9Zh9HxR.png"), (832, 1344)) ,
+                      "frameSize": {"w": 0, "h": 0}       
                       },
               "moveSets": {
-                           #iniY, totalF
-                           "walkLeft": [9, 9],
-                           "walkDown": [10, 9]
+                           #iniY, totalF, iniF
+                           "walkLeft": [9, 9, 0],
+                           "walkRight": [11, 9, 0],
+                           "walkUp": [8, 9, 1],
+                           "walkDown": [10, 9, 1]
                            },
-              "ttC": [2][0]
+              "actMove": ["walkDown", 0],
+              "ttC": [1, 0]
               }
     scene = {"visibles": {"leftColumn": 0,
                           "RightColumn": 0,
@@ -44,8 +46,10 @@ class stage(object):
         pygame.init()
         self.stage = pygame.display.set_mode(self.stgConf['resolution'], HWSURFACE|DOUBLEBUF)
         pygame.display.set_caption("TRAIN.ME")
+        self.avatar["sprite"]["frameSize"]["w"] = self.avatar["sprite"]["dimensions"]["w"]/self.avatar["sprite"]["nums"]["x"]
+        self.avatar["sprite"]["frameSize"]["h"] = self.avatar["sprite"]["dimensions"]["h"]/self.avatar["sprite"]["nums"]["y"]
         self.loadScene()
-        
+                
         while True:
             for event in pygame.event.get():
                 if(event.type==QUIT):
@@ -66,25 +70,30 @@ class stage(object):
         if(276 in self.keyPressed and self.ttP[276][1]==0):
             if self.avatar['posi']['x']>0 and self.sceneData["tiles"][self.avatar['posi']['x']-1][self.avatar['posi']['y']]["W"]==1:
                 self.avatar['posi']['x'] -=1
+                self.avatar["actMove"][0] = "walkLeft"
         if(275 in self.keyPressed and self.ttP[275][1]==0):
             if self.avatar['posi']['x']<self.sceneData["conf"]["dimensions"][0]-1 and self.sceneData["tiles"][self.avatar['posi']['x']+1][self.avatar['posi']['y']]["W"]==1:
                 self.avatar['posi']['x'] +=1
+                self.avatar["actMove"][0] = "walkRight"
         if(273 in self.keyPressed and self.ttP[273][1]==0):
             if self.avatar['posi']['y']>0 and self.sceneData["tiles"][self.avatar['posi']['x']][self.avatar['posi']['y']-1]["W"]==1:
                 self.avatar['posi']['y'] -=1
+                self.avatar["actMove"][0] = "walkUp"
         if(274 in self.keyPressed and self.ttP[274][1]==0):
             if self.avatar['posi']['y']<self.sceneData["conf"]["dimensions"][1]-1 and self.sceneData["tiles"][self.avatar['posi']['x']][self.avatar['posi']['y']+1]["W"]==1:
                 self.avatar['posi']['y'] +=1
+                self.avatar["actMove"][0] = "walkDown"
                 
         for i in self.keyPressed:
-            if(self.ttP[i][1]==0):
+            if(self.ttP.has_key(i) and 1 in self.ttP[i] and self.ttP[i][1]==0):
                 self.ttP[i][1] = self.ttP[i][0]
-            else:
+            elif(self.ttP.has_key(i) and 1 in self.ttP[i]):
                 self.ttP[i][1] -= 1
     
     def setKeyEvents(self, event):
         for i in self.keyPressed:
-            self.ttP[i][1] = 0
+            if(self.ttP.has_key(i) and 1 in self.ttP[i]):
+               self.ttP[i][1] = 0
         if(event.type==KEYDOWN):
             self.keyPressed[event.key] = True
         elif(self.keyPressed.has_key(event.key)):
@@ -99,7 +108,6 @@ class stage(object):
         self.setRowFromStr()
         
         for i in self.sceneData["loads"]["tiles"]:
-            #self.resources["tiles"][i] = pygame.image.load(self.sceneData["loads"]["tiles"][i][0])
             self.resources["tiles"][i] = [pygame.transform.scale(pygame.image.load(self.sceneData["loads"]["tiles"][i][0]), 
                                                                 (self.sceneData["loads"]["tiles"][i][1], self.sceneData["loads"]["tiles"][i][2])
                                                                 ),
@@ -123,27 +131,28 @@ class stage(object):
                     endC = self.colString[i['end_col']]
                 else:
                     endC = i['end_col']
-                if isinstance(i['end_col'], basestring):
+                if isinstance(i['end_row'], basestring):
                     endR = self.colString[i['end_row']]
                 else:
                     endR = i['end_row']
                     
                 for rX in range(iniC, endC):
-                    self.ranges[str(rX)] = {}
+                    if not(self.ranges.has_key(str(rX))):
+                        self.ranges[str(rX)] = {}
                     for rY in range(iniR, endR):
-                        #print(str(rX)+" - "+str(rY))
                         self.ranges[str(rX)][str(rY)] = {}
                         self.ranges[str(rX)][str(rY)]["T"] = i['T']
                         self.ranges[str(rX)][str(rY)]["W"] = i['W']
-                #print(self.ranges)
+                        if(i.has_key("L")):
+                            self.ranges[str(rX)][str(rY)]["L"] = i['L']
                         
     def setColFromStr(self):
         self.colString = {"first_negative": self.stgConf['tilesX']*-1, "last_negative": -1,
-                          "firts_positive": 0, "last_positive": self.sceneData["conf"]["dimensions"][0]*2-1}
+                          "first_positive": 0, "last_positive": self.sceneData["conf"]["dimensions"][0]}
 
     def setRowFromStr(self):
-        self.colString = {"first_negative": self.stgConf['tilesY']*-1, "last_negative": -1,
-                          "firts_positive": 0, "last_positive": self.sceneData["conf"]["dimensions"][1]*2-1}
+        self.rowString = {"first_negative": self.stgConf['tilesY']*-1, "last_negative": -1,
+                          "first_positive": 0, "last_positive": self.sceneData["conf"]["dimensions"][1]}
     
     def setAvatar(self):
         s = pygame.Surface((self.stgConf['tileSize'][0], self.stgConf['tileSize'][1]), pygame.SRCALPHA, 32)
@@ -157,17 +166,20 @@ class stage(object):
                      ),
                  1
                  )
-        self.stage.blit(self.avatar["sprite"]["file"], rect)
         
-        """pygame.draw.rect(self.stage, 
-             (255, 255, 0), 
-                (int(math.floor(self.stgConf['tilesX']/2))*self.stgConf['tileSize'][0] +self.stgConf['adj']['x'], 
-                 int(math.floor(self.stgConf['tilesY']/2))*self.stgConf['tileSize'][1] +self.stgConf['adj']['y'], 
-                 self.stgConf['tileSize'][0], 
-                 self.stgConf['tileSize'][1]
-                 )
-             )"""
-    
+        if(self.avatar["ttC"][1]==0):
+            if(self.avatar["actMove"][1]<self.avatar["moveSets"][self.avatar["actMove"][0]][1]-1):
+                self.avatar["actMove"][1] +=1
+            else:
+                self.avatar["actMove"][1] = self.avatar["moveSets"][self.avatar["actMove"][0]][2]
+            self.avatar["ttC"][1] = self.avatar["ttC"][0]
+        else:
+            self.avatar["ttC"][1] -=1
+            
+        x = self.avatar["sprite"]["frameSize"]["w"] * self.avatar["actMove"][1]
+        y = self.avatar["moveSets"][self.avatar["actMove"][0]][0] * self.avatar["sprite"]["frameSize"]["h"]
+        self.stage.blit(self.avatar["sprite"]["file"], rect, (x, y, self.avatar["sprite"]["frameSize"]["w"], self.avatar["sprite"]["frameSize"]["h"]))
+        
     def setVisibles(self):
         self.scene['visibles']['leftColumn'] = self.avatar['posi']['x'] - int(math.floor(self.stgConf['tilesX']/2))
         self.scene['visibles']['topLine'] = self.avatar['posi']['y'] - int(math.floor(self.stgConf['tilesY']/2))
@@ -208,8 +220,8 @@ class stage(object):
             xReal = self.scene['visibles']['leftColumn']+x
             for y in range(self.stgConf['tilesY']):
                 yReal = self.scene['visibles']['topLine']+y
-                xS = (self.stgConf['adj']['x'] + x*self.stgConf['tileSize'][0]) - (self.stgConf['tileSize'][0]/2*y) - (self.stgConf['tileSize'][0]/2*x) + xBase - self.stgConf['tileSize'][0]/2
-                yS = (self.stgConf['adj']['y'] + y*self.stgConf['tileSize'][1]) + (self.stgConf['tileSize'][1]/2*x) - (self.stgConf['tileSize'][1]/2*y) + yBase + self.stgConf['tileSize'][1]/2
+                xS = (self.stgConf['adj']['x'] + x*self.stgConf['tileSize'][0]) - (self.stgConf['tileSize'][0]/2*y) - (self.stgConf['tileSize'][0]/2*x) + xBase - self.stgConf['tileSize'][1]/1
+                yS = (self.stgConf['adj']['y'] + y*self.stgConf['tileSize'][1]) + (self.stgConf['tileSize'][1]/2*x) - (self.stgConf['tileSize'][1]/2*y) + yBase + self.stgConf['tileSize'][0]/1
                  
                 self.grid[x][y] = {"R": (xReal, yReal)}
                 
@@ -221,83 +233,30 @@ class stage(object):
                             l = self.sceneData["tiles"][self.grid[x][y]['R'][0]][self.grid[x][y]['R'][1]]["L"]
                             if not(self.resources["overW"].has_key(l)):
                                 self.resources["overW"][l] = {}
-                                
-                            self.resources["overW"][l][len(self.resources["overW"][l])] = {
-                                "x": xS,
-                                "y": yS,
-                                "im": resImg
-                            }
+                            self.resources["overW"][l][len(self.resources["overW"][l])] = {"x": xS, "y": yS, "im": resImg}
                             continue
                             
                         rect = pygame.draw.rect(s, 
-                        #rect = pygame.draw.rect(self.stage,
-                                 (255, 255, 255), 
-                                    (xS + resImg[3], 
-                                     yS - resImg[2] + resImg[4], 
-                                     resImg[1],
-                                     resImg[2]
-                                     ),
-                                 1
-                                 )
+                                (255, 255, 255), 
+                                (xS + resImg[3], yS - resImg[2] + resImg[4], resImg[1], resImg[2]),
+                                1)
                         self.stage.blit(resImg[0], rect)
-                        #print(self.resources["tiles"][str(self.sceneData["tiles"][self.grid[x][y]['R'][0]][self.grid[x][y]['R'][1]]["T"])].get_height())
 
-                #print(self.ranges)
-                #if(str(xReal) in self.ranges and str(yReal) in self.ranges[str(xReal)]):
-                    #continue
-                    #print(str(xReal))
-                    #print(self.ranges[xReal])
-                    #self.stage.blit(self.resources["tiles"][str(self.ranges[str(xReal)][str(yReal)]["T"])], rect)
+                if(str(xReal) in self.ranges and str(yReal) in self.ranges[str(xReal)]):
+                    resImg = self.resources["tiles"][str(self.ranges[str(xReal)][str(yReal)]["T"])]
+                    
+                    if(self.ranges[str(xReal)][str(yReal)].has_key("L")):
+                        l = self.ranges[str(xReal)][str(yReal)]["L"]
+                        if not(self.resources["overW"].has_key(l)):
+                            self.resources["overW"][l] = {}
+                        self.resources["overW"][l][len(self.resources["overW"][l])] = {"x": xS, "y": yS, "im": resImg}
+                        continue 
+                                       
+                    rect = pygame.draw.rect(s, 
+                                 (255, 255, 255), 
+                                 (xS + resImg[3], yS - resImg[2] + resImg[4], resImg[1], resImg[2]),
+                                 1)
+                    self.stage.blit(resImg[0], rect)
                                        
                 #DEBUG TEXT---------------------------------------------------------------------------
-                """text = tileId.render("V: "+str(x)+" "+str(y), True, (255,0,0))
-                textRect = text.get_rect()
-                textRect.centerx = xS + 15
-                textRect.centery = yS + 5
-                self.stage.blit(text, textRect)
-                
-                text = tileId.render("R:"+str(self.scene['visibles']['leftColumn']+x)+" "+str(self.scene['visibles']['topLine']+y), True, (255,0,0))
-                textRect = text.get_rect()
-                textRect.centerx = xS + 15
-                textRect.centery = yS + 12
-                self.stage.blit(text, textRect)"""
-                #-------------------------------------------------------------------------------------
-                
-                #pygame.transform.rotate(rect, 45)
-        
 stage = stage()
-
-"""
-
-BLACK = (0,0,0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-basicFont = pygame.font.SysFont(None, 48)
-
-text = basicFont.render("Hello world!", True, WHITE, BLUE)
-textRect = text.get_rect()
-textRect.centerx = windowSurface.get_rect().centerx
-textRect.centery = windowSurface.get_rect().centery
-
-windowSurface.fill(WHITE)
-
-pygame.draw.polygon(windowSurface, GREEN, ((146,0), (291,106), (236,277), (56,277), (0,106)))
-
-pygame.draw.line(windowSurface, BLUE, (60,60), (120,60), 4)
-pygame.draw.line(windowSurface, BLUE, (120,60), (60,120))
-pygame.draw.line(windowSurface, BLUE, (60,120), (120,120), 4)
-
-pygame.draw.circle(windowSurface, BLUE, (300,50), 20, 0)
-
-pygame.draw.ellipse(windowSurface, RED, (300,250,40,80), 1)
-
-pixArray = pygame.PixelArray(windowSurface)
-pixArray[480][380] = BLACK
-del pixArray
-
-windowSurface.blit(text, textRect)
-"""
-
